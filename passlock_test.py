5f99fb7dd151befe687ab9021fe25096b27abd99
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-23 11:34:34 +0300

    test case to test if a new user instance has been saved into the User list

--- passlock_test.py
+++ passlock_test.py
@@ -18,3 +18,11 @@ class TestClass(unittest.TestCase):
         """
         self.assertEqual(self.new_user.username,'OwitiCharles')
         self.assertEqual(self.new_user.password,'XyZ3thf1')
+
+    def test_save_user(self):
+        """
+        test case to test if a new user instance has been saved into the User list
+
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
