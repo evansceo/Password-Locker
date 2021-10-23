27c1ecc8fc3baf6796feb8703910c839dca80557
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-23 11:37:42 +0300

    A test class that defines test cases for credentials class

--- passlock_test.py
+++ passlock_test.py
@@ -26,3 +26,9 @@ class TestClass(unittest.TestCase):
         """
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+
+    """ 
