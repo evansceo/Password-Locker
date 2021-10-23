5088627a54c9d0f836458e1bdb8fd7c9ea678754
evansceo <evans.onyiego@student.moringaschool.com>
2021-10-23 12:23:32 +0300

    method that displays all the credentials that has been saved by the user

--- passlock_test.py
+++ passlock_test.py
@@ -102,3 +102,13 @@ class TestCredentials(unittest.TestCase):
         the_credential.save_details()
         credential_is_found = Credentials.if_credential_exist("Twitter")
         self.assertTrue(credential_is_found)
+
+    def test_display_all_saved_credentials(self):
+        '''
+        method that displays all the credentials that has been saved by the user
+        '''
+
+        self.assertEqual(Credentials.display_credentials(),Credentials.credentials_list)
+
+if __name__ == "__main__":
+    unittest.main()
